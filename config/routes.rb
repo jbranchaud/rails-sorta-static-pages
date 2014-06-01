20dc5642d5190adc8a5fced36d81cc99eb2d7bd1
jbranchaud <jbranchaud@gmail.com>
2014-06-01 15:50:59 -0300

    Update the routes to use the URLs we want with the match method.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 RailsStaticPages::Application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  get "pages/pricing"
-  get "pages/contact"
+
+  # We start with a bunch of simple get routes for our pages, rails was nice
+  # enough to generate these for us.
+  # get "pages/home"
+  # get "pages/about"
+  # get "pages/pricing"
+  # get "pages/contact"
+  
+  # We can do better than the simple get routes by specifying the custom
+  # URLs we want to use which will be matched to their respective methods in
+  # the pages_controller. We will specify that this is to be done via a get
+  # request.
+  match "/home",    to: "pages#home",    via: 'get'
+  match "/about",   to: "pages#about",   via: 'get'
+  match "/pricing", to: "pages#pricing", via: 'get'
+  match "/contact", to: "pages#contact", via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
