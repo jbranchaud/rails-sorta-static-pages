f6923d129d346d5128aa5fd5441f0b7b80381268
jbranchaud <jbranchaud@gmail.com>
2014-06-01 23:07:14 -0300

    Add a full_title method to the application helper for use by views.
    The full_title method takes a string and attempts to generate a full_title
    to be used as the calling page's title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    generic_title = "Rails Sorta Static Pages"
+    if page_title
+      "#{page_title} | #{generic_title}"
+    else
+      generic_title
+    end
+  end
+
 end
