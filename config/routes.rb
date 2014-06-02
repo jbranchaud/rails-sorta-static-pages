22a8c591c25a45a90d4d5be49f4cb61efb0f86bc
jbranchaud <jbranchaud@gmail.com>
2014-06-01 21:33:53 -0300

    Update the way the routes for the static pages is done.

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,17 @@ RailsStaticPages::Application.routes.draw do
   # URLs we want to use which will be matched to their respective methods in
   # the pages_controller. We will specify that this is to be done via a get
   # request.
-  match "/home",    to: "pages#home",    via: 'get'
-  match "/about",   to: "pages#about",   via: 'get'
-  match "/pricing", to: "pages#pricing", via: 'get'
-  match "/contact", to: "pages#contact", via: 'get'
+  # match "/home",    to: "pages#home",    via: 'get'
+  # match "/about",   to: "pages#about",   via: 'get'
+  # match "/pricing", to: "pages#pricing", via: 'get'
+  # match "/contact", to: "pages#contact", via: 'get'
+
+  # With Rails 4, we can use get
+  get "/home",    to: "pages#home"
+  get "/about",   to: "pages#about"
+  get "/pricing", to: "pages#pricing"
+  get "/contact", to: "pages#contact"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
